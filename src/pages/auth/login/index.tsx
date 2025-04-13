ff1a15ee436b26dcfb1266512dc3f9ed1f45eebd
Manish rawat <manish.rawat@essencesoftwares.com>
2025-04-13 17:15:30 +0530

    shadcn component added

--- src/pages/auth/login/index.tsx
+++ src/pages/auth/login/index.tsx
@@ -1,10 +1,10 @@
 import '../styles.scss'
+import { Button } from '@/components/ui/button'
 const LoginPage = () => {
   return (
     <div className="auth-page">
       <h1 className="text-mint-500">Login Page</h1>
-      <p>Welcome to the login page!</p>
-      <p>Please enter your credentials to log in.</p>
+      <Button variant={'outline'}>Click me</Button>
     </div>
   )
 }
